252a017faa5c628000bb8d6dd583d094efbdc292
hhc <441821832@qq.com>
2019-07-19 13:55:35 +0800

    修改

--- July/src/com/neuedu/test/Test.java
+++ July/src/com/neuedu/test/Test.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        /*Math.sqrt(4);//求根号4，返回的结果是double型
+        Math.sqrt(4);//求根号4，返回的结果是double型
         Math.random();//生成0~1之间的随机数
         Math.max(2,3);//求俩个类型相同的数的最大值
         Math.min(3,4);//求俩个类型相同的数的最小值
@@ -15,7 +15,7 @@ public class Test {
         Math.round(4.5);//四舍五入
         Math.floor(4.4);//向下取整
         Math.ceil(3.7);//向上取整
-        Math.pow(2,3);//2的3次方*/
+        Math.pow(2,3);//2的3次方
         int[] a={1,2,3,4,5,6,7};
         Arrays.sort(a);//排序
         int[] b=Arrays.copyOf(a,9);//复制数组
